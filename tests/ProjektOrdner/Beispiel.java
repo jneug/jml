92d663d38b3797a2b1cec10fdd7e64cbcffb4db3
J. Neugebauer <ngb@helmholtz-bi.de>
2022-01-19 11:49:27 +0100

    Added some example and test files

---
+++ tests/ProjektOrdner/Beispiel.java
@@ -0,0 +1,26 @@
+class Example {
+    /*aufgabe*
+    // TODO: Erstelle eine Objektvariable "zahl" vom Typ int
+    *aufgabe*/
+    //<loesung>
+    private int zahl;
+    //</loesung>
+
+    public int add( int pAndereZahl ) {
+        /*aufgabe*
+        // TODO: Gib die Summe aus der Objektvariablen "zahl" und "pSummand" zurück.
+        return 0;
+        *aufgabe*/
+        //<loesung>
+        return zahl + pAndereZahl;
+        //</loesung>
+    }
+
+    /*aufgabe* 2
+    public int sub( int pAndereZahl ) {
+        // TODO: Gib die Differenz aus der Objektvariablen "zahl" und "pSummand" zurück.
+        return 0;
+    }
+    *aufgabe*/
+
+}
