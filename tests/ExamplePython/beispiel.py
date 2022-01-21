3a3a37b72fb0e0bd175227eef3aa0785e17f1c00
J. Neugebauer <ngb@helmholtz-bi.de>
2022-01-21 13:53:33 +0100

    Added new examples (python, html)

---
+++ tests/ExamplePython/beispiel.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+
+#<aufg>
+## TODO: Erstelle eine Liste mit den Namen aller Schülerinnen und Schüler im Kurs.
+#names =
+#</aufg>
+#<ml>
+names = [
+    "Max Mustermann",
+    "Hansi Haselmaus"
+]
+#</ml>
+
+#<aufg>
+## TODO: Gib die Liste der Namen auf der Konsole aus.
+#</aufg>
+#<aufg> 2
+#for n in names:
+#    pass
+#</aufg>
+#<ml>
+for n in names:
+    println(n)
+#</ml>
