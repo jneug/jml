cac7709628cc667f1552e59684c90b1688e6105d
J. Neugebauer <ngb@helmholtz-bi.de>
2022-03-04 16:48:55 +0100

    Beispielprojekt für erweiterte ML-Tags

---
+++ tests/ExtendedML/Beispiel.java
@@ -0,0 +1,29 @@
+class Beispiel {
+    /*aufg*
+    // TODO: Erstelle eine Objektvariable "zahl" vom Typ int
+    *aufg*/
+    //ml* 3
+    private int zahl;
+    //*ml
+
+    public int add( int pAndereZahl ) {
+        /*aufg* !=3
+        // TODO: Gib die Summe aus der Objektvariablen "zahl" und "pSummand" zurück.
+        return 0;
+        *aufg*/
+        //ml* 3
+        return zahl + pAndereZahl;
+        //*ml
+    }
+    /*aufg* >=2
+
+    public int sub( int pAndereZahl ) {
+        // TODO: Gib die Differenz aus der Objektvariablen "zahl" und "pSummand" zurück.
+        return 0;
+    }
+    *aufg*/
+
+	/*aufg* 3
+	// Nur in Version 3
+	*aufg*/
+}
